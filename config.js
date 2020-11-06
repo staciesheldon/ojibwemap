187676b88dd17c402da73243313285d874c6d8e2
staciesheldon <34325370+staciesheldon@users.noreply.github.com>
2020-11-06 15:39:05 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 
 const config = {
     style: "mapbox://styles/mapbox/light-v10",
-    accessToken: "Your Access Token",
-    CSV: "./Example.csv",
-    center: [-120.234, 47.398], //Lng, Lat
-    zoom: 6, //Default zoom
+    accessToken: "pk.eyJ1Ijoic3RhY2llc2hlbGRvbiIsImEiOiJja2d6dmwwajYwOXZ5MnhzaWZwdzhpZ3hrIn0.20fQRir-1YxbYtiy4yLzZw",
+    CSV: "https://docs.google.com/spreadsheets/d/1SbtQ2_BaUH6HrzNP8WhRYAmoEG0l31LBM7pDf5L2liE/gviz/tq?tqx=out:csv&sheet=Nations",
+    center: [-85.002462, 45.246623], //Lng, Lat
+    zoom: 5, //Default zoom
     title: "Replace with your title",
     description: "Replace with information about your application. Ex. You can search by address to sort the list below by distance. You can also filter the list by language support options, which days a location is open, and whether they have devices to use to complete the survey by phone or online.",
     sideBarInfo: ["Header", "Info 1", "Info 2"],

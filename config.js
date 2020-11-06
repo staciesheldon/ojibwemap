e0ccecef1c49581e572ae1953e42f8782e71253e
staciesheldon <34325370+staciesheldon@users.noreply.github.com>
2020-11-06 15:49:57 -0500

    Update config.js

--- config.js
+++ config.js
@@ -5,45 +5,26 @@ const config = {
     CSV: "https://docs.google.com/spreadsheets/d/1SbtQ2_BaUH6HrzNP8WhRYAmoEG0l31LBM7pDf5L2liE/gviz/tq?tqx=out:csv&sheet=Nations",
     center: [-85.002462, 45.246623], //Lng, Lat
     zoom: 5, //Default zoom
-    title: "Replace with your title",
+    title: "Nations of the Great Lakes Watershed",
     description: "Replace with information about your application. Ex. You can search by address to sort the list below by distance. You can also filter the list by language support options, which days a location is open, and whether they have devices to use to complete the survey by phone or online.",
-    sideBarInfo: ["Header", "Info 1", "Info 2"],
-    popupInfo: ["Popup Information"],
+    sideBarInfo: ["Nation", "AnishinaabeName", "Translation"],
+    popupInfo: ["Nation"],
     filters: [
         {
             type: "dropdown",
-            title: "Title of filter: ",
-            columnHeader: "Column Name",
+            title: "In the Watershed?: ",
+            columnHeader: "Watershed",
             listItems: [
-                'filter one',
-                'filter two',
-                'filter three',
-                'filter four',
-                'filter five',
-                'filter six',
-                'filter seven'
+                'Y',
+                'N',
             ]
         },
         {
             type: "checkbox",
-            title: "Title of filter: ",
-            columnHeader: "Column Name",
-            listItems: ["filter one", "filter two", "filter three"]
+            title: "Group: ",
+            columnHeader: "Group",
+            listItems: ["Ojibwe", "Odawa", "Potatowatomi"]
         },
-        {
-            type: "dropdown",
-            title: "Title of filter: ",
-            columnHeader: "Column Name",
-            listItems: [
-                'filter one',
-                'filter two',
-                'filter three',
-                'filter four',
-                'filter five',
-                'filter six',
-                'filter seven'
-            ]
-        }
     ]
 
 };

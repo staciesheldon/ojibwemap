fdc869cd05eb33e298011a265e944d4c790e75aa
staciesheldon <34325370+staciesheldon@users.noreply.github.com>
2020-11-10 18:20:51 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 
 const config = {
-    style: "mapbox://styles/mapbox/light-v10",
+    style: "mapbox://styles/staciesheldon/ckhbhgzys0n5d19p62i91e03u", 
     accessToken: "pk.eyJ1Ijoic3RhY2llc2hlbGRvbiIsImEiOiJja2d6dmwwajYwOXZ5MnhzaWZwdzhpZ3hrIn0.20fQRir-1YxbYtiy4yLzZw",
     CSV: "https://docs.google.com/spreadsheets/d/1SbtQ2_BaUH6HrzNP8WhRYAmoEG0l31LBM7pDf5L2liE/gviz/tq?tqx=out:csv&sheet=Nations",
     center: [-85.002462, 45.246623], //Lng, Lat

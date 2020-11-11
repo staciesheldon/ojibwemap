762ea33651f2b8069094933c1a20dace1e4eb7c1
staciesheldon <34325370+staciesheldon@users.noreply.github.com>
2020-11-10 20:13:00 -0500

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ const config = {
     CSV: "https://docs.google.com/spreadsheets/d/1SbtQ2_BaUH6HrzNP8WhRYAmoEG0l31LBM7pDf5L2liE/gviz/tq?tqx=out:csv&sheet=Nations",
     center: [-85.002462, 45.246623], //Lng, Lat
     zoom: 4, //Default zoom
-    title: "Nations of the Great Lakes Watershed",
+    title: "Anishinaabewakiing - Anishinaabe Territory",
     description: "View and filter tribal nations by their location in the Watershed.",
     sideBarInfo: ["Nation", "AnishinaabeName", "Translation"],
     popupInfo: ["Nation"],

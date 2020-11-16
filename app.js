493596f66e7f7f1f8e18a0f6065a86d5856cf52b
staciesheldon <34325370+staciesheldon@users.noreply.github.com>
2020-11-16 10:45:20 -0500

    Update app.js

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ function createPopup(currentFeature) {
     if (popups[0]) popups[0].remove();
     const popup = new mapboxgl.Popup({ closeOnClick: true })
         .setLngLat(currentFeature.geometry.coordinates)
-        .setHTML("<h3>" + '<a href="' + currentFeature.properties[config.popupInfo[3]] + '" target="_blank" class="popupLink">' + currentFeature.properties[config.popupInfo[0]] + "</a>" + "</h3>" + "<br>" + currentFeature.properties[config.popupInfo[1]] + "<br>"  + "<h5>" + currentFeature.properties[config.popupInfo[2]] + "</h5>")
+        .setHTML("<h3>" + '<a href="' + currentFeature.properties[config.popupInfo[3]] + '" target="_blank" class="popupLink">' + currentFeature.properties[config.popupInfo[0]] + "</a>" + "</h3>" + "<br>" + currentFeature.properties[config.popupInfo[1]] + "<br>"  + '<h5 class="addressPopup">' + currentFeature.properties[config.popupInfo[2]] + "</h5>")
         .addTo(map);
 }
 
